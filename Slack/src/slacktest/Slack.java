ba420de7ca3cc4d25173655d21b1722b70db7657
VG00445412 <VG00445412@techmahindra.com>
2017-01-19 18:15:58 +0530

    Commit

--- Slack/src/slacktest/Slack.java
+++ Slack/src/slacktest/Slack.java
@@ -4,8 +4,7 @@ public class Slack {
 	public static void main(String[] args)
 	{
 		System.out.println("Hello world");
-		System.out.println("Hello world");
-		System.out.println("Hello world");
+		System.out.println("Hello");
 	}
 
 }
